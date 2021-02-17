c06720d1e0c9c4eee3b6e4981afd29fd098285c1
Michael McMasters <jmcmasters411@gmail.com>
2021-02-17 12:25:53 -0500

    Set up project structure

---
+++ src/main/java/com/mcmasters/notecards/NotecardsApplication.java
@@ -0,0 +1,12 @@
+package com.mcmasters.notecards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotecardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotecardsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mcmasters/notecards/NotecardsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mcmasters.notecards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotecardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
